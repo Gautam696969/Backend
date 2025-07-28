090e8b3f39dcfe16e7eb95d70b688c54136ccabb
Gautam696969 <palgautam69@gmail.com>
2025-07-28 18:02:54 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import dotenv from 'dotenv'
+import connectDB from './Db/index.js'
+
+dotenv.config( {
+    path: './env'
+} )
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from 'express';
+// const app = express()
+
+//     ( async () => {
+//         try {
+//             await mongoose.connect( `${process.env.MONGODB_URI}/${DB_Name}` )
+//             app.on( "error", ( error ) => {
+//                 console.log( "ERROR", error );
+//                 throw error
+//             } )
+
+//             app.listen( process.env.PORT, () => {
+//                 console.log( `App is listening on YOUR PORT${process.env.PORT}` )
+//             } )
+//         } catch ( error ) {
+//             console.log( "DATABASE CONNECTION ERROR", error )
+//         }
+//     } )()
